664104b214c212778889415a3b35f8b1ef397caa
Lucas Bardeli de Bastiani Silva <lucas.silva1111@fatec.sp.gov.br>
2025-03-20 12:49:04 -0300

    Salvando

---
+++ aula-20-03-forms-js/js/calculadora.js
@@ -0,0 +1,12 @@
+
+const form = document.getElementById('formNumero')
+var inputNum1 = document.getElementById('inputNumero1')
+var inputNum2 = document.getElementById('inputNumero2')
+var inputOperacao = document.getElementById('inputOperacao')
+const lista = document.getElementById('listaResultados')
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    
+});
--- aula-20-03-forms-js/js/script.js
+++ aula-20-03-forms-js/js/script.js
@@ -1,29 +1,43 @@
 
+// const define a variável como constante (não pode ser alterada, 
+// neste caso, não pode trocar de elemento do html)
+
 const form = document.getElementById('formTarefa');
 const input = document.getElementById('inputTarefa');
-const lista = document.getElementById('listaTarefa');
+const lista = document.getElementById('listaTarefas');
+
+// addEventListener adiciona uma "escuta" a um evento de elemento html
+// 'subit' é o evento que ocorre quando o formulário é enviado
+// function(event) cria a função que será executada quando o evento ocorrer
+// event é a variável que armazena as informações do evento que ocorreu  
 
 form.addEventListener('submit', function(event) {
+    // Evita que o formulário seja enviado e a página recarregada
     event.preventDefault();
 
-    const textoTarefa = input.value.trim();
+    // Armazena o valor do input na variável
+    const textoTarefa = input.value.trim(); // trim() remove os espaços vazios do começo e do fim do texto
 
     if (textoTarefa != '') {
+        // Cria um elemento html do tipo li
         const li = document.createElement('li');
+        // Adiciona o texto do input no elemento li
         li.textContent = textoTarefa;
 
         const botaoRemover = document.createElement('button');
         botaoRemover.textContent = 'X';
+        // Adiciona a classe remover ao botão criado
         botaoRemover.classList.add('remover');
 
         botaoRemover.addEventListener('click', function() {
+            // Remove o elemento li da lista se o botão for clicado
             lista.removeChild(li);
         });
-
+        // Adiciona o botão Remover ao elemento li
         li.appendChild(botaoRemover);
-
+        // Adiciona o elemento li a lista de tarefas
         lista.appendChild(li);
-
+        // Limpa o valor do input
         input.value = '';
     }
 });
